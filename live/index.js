05f87ad8988f955069985b04431df818dc766afd
zmgamer <85769761+ZmGAm@users.noreply.github.com>
2024-08-04 12:14:21 +0500

    Update index.js

--- live/index.js
+++ live/index.js
@@ -14,11 +14,11 @@ const app = express()
 
 app.use(cors(
 
-  // {
-  //   origin:[""],
-  //   methods:["POST","GET"],
-  //   credential:true
-  // }
+  {
+    origin:["*"],
+    methods:["GET","POST","PUT","DELETE","OPTION"],
+    credential:true.
+  }
 ))
 
 app.get("/",(req,res)=>{
